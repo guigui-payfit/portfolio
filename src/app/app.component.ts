efa3e497042d8c79dfc3e068b8f2666844fcfafd
Guillaume CORNU <guillaume@payfit.com>
2024-05-03 16:32:52 +0200

    feat(portfolio): add first section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "app-root",
   standalone: true,
   styleUrl: "./app.component.scss",
